bd5ed6efd14371fbd7a4453d47a0847cdaedaab9
Artem Gusev <agusev2311@mail.ru>
2024-10-12 14:29:50 +0300

    Something
    - Move /static to /server/static
    - Added config (config.json)
    - Added db (data.sqlite)

--- server/app.py
+++ server/app.py
@@ -4,8 +4,27 @@ import flask
 from flask import Flask, render_template, request, send_from_directory, jsonify
 import requests
 import os
+import json
 
 app = flask.Flask(__name__)
+
+with open('config.json', 'r') as file:
+    config = json.load(file)
+print(config)
+
+conn = sqlite3.connect('data.sqlite')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS requests (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        messages TEXT,
+        response TEXT,
+        user_id TEXT,
+        time TEXT
+    )
+''')
+conn.commit()
+conn.close()
 
 @app.route('/')
 def favicon():
@@ -22,8 +41,11 @@ def login():
         'age': 30,
         'city': 'Wonderland'
     }
-    
     return jsonify(data)
 
+@app.route("/server-config")
+def server_config():
+    return jsonify(config)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
